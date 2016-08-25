8339a890bb679161fa03cec88214b1b58fb7da71
bpasha7 <bpasha@mail.ru>
2016-08-26 02:13:17 +0700

    Complex oid calculation

--- controllers/monitor.php
+++ controllers/monitor.php
@@ -3,29 +3,20 @@ class Monitor extends Controller
 {
     public function __construct()
     {
-        //echo "Контроллер обработки ошибок";
         parent::__construct();
-        //$this->view->msg = 'Страницы не существует != (';
-        //$this->view->render('error / index');
-        //$this->view->render('monitor/index');
-        //$this->model->refresh();
     }
     public function on()
     {
-        //$this->view->msg = 'Страницы не существует999!';
         $this->view->render('monitor/index');
        
     }
     public function update()
     {
-        //$this->view->msg = 'Страницы не существует999!';
         $this->model->update();
        
     }
     public function refresh()
     {
-        //$this->view->msg = 'Страницы не существует999!';
-       // $this->view->render('monitor/index');
        $this->model->refresh();
     }
     public function create()
@@ -39,11 +30,62 @@ $headers = 'From: test <s.ql@bk.ru>' . "\r\n" .
 
 echo mail($to, $subject, $message, $headers);
     }
+   /* private function parse($str)
+    {
+		$s = $str;
+		$p = array("/","*","+","-");
+		$start = 0;
+		for($i = 0; $i < strlen($str); $i++){
+			for($j = 0; $j < count($p); $j++){
+				if($str[$i] == $p[$j] || $i == strlen($str) - 1){
+					if($i == strlen($str) - 1)
+					$c = substr($str,$start,$i - $start + 1);
+					else
+					$c = substr($str,$start,$i - $start);
+					$c = str_replace(array("(",")"), "", $c);
+					$s     = str_replace($c,'ok',$s);
+					$start = $i + 1;
+					$i     = $start;
+				}
+			}
+		}+
+		echo $s;
+		return $s;
+	}*/
     public function test()
     {
-		$ma ="2+10";
+		/*$ma ="89*10";
 $p = eval('return '.$ma.';');
+print $p;*/
+
+$str='10';
+$s=$str;
+$p = array("/","*","+","-");
+$start=0;
+for($i=0; $i<strlen($str); $i++)
+{
+	for($j=0; $j<count($p); $j++)
+	{
+		if($str[$i]==$p[$j]||$i==strlen($str)-1)
+		{
+			if($i==strlen($str)-1)
+				$c= substr($str,$start,$i-$start+1);
+			else
+				$c= substr($str,$start,$i-$start);
+				//
+			$c = str_replace(array("(",")"), "", $c);
+			if(!is_numeric($c))
+				$s=str_replace($c,'70',$s);
+			$start = $i+1;
+			$i=$start;
+			//echo $s;
+		}
+	}
+}
+//echo $s;
+$p = eval('return '.$s.';');
 print $p;
+return $s;
 	}
 }
 ?>
--- models/monitor_model.php
+++ models/monitor_model.php
@@ -62,6 +62,41 @@ class Monitor_Model extends Model
  	{
 		
 	}*/
+	private function parse($str, $row)
+    {
+		$s = $str;
+		$p = array("/","*","+","-");
+		$start = 0;
+		$simple=TRUE;
+		for($i = 0; $i < strlen($str); $i++){
+			for($j = 0; $j < count($p); $j++){
+				if($str[$i] == $p[$j] || $i == strlen($str) - 1){
+					if($i == strlen($str) - 1)
+						$c = substr($str,$start,$i - $start + 1);
+					else
+						{
+							$c = substr($str,$start,$i - $start);
+							$simple=FALSE;
+						}
+					$c = str_replace(array("(",")"), "", $c);
+					$res = snmpget($row['DeviceIP'], $row['Community'], $c, $row['Timeout']);
+					$res = substr($res, strpos($res,":")+1, strlen($res));
+					if(!is_numeric($c))
+						$s     = str_replace($c, $res ,$s);
+					$start = $i + 1;
+					$i = $start;
+					
+				}
+			}
+		}	
+		//echo $s;
+		if($simple)
+			$p = snmpget($row['DeviceIP'], $row['Community'], $str, $row['Timeout']);
+		else
+			$p = eval('return '.$s.';');
+		//print $p;
+		return $p;
+	}
     public function refresh()
     {
 		$sth = $this->database->prepare("CALL GetDevices(".$_POST['from'].", ".$_POST['cnt'].", ".$_POST['grp'].")");
@@ -73,11 +108,15 @@ class Monitor_Model extends Model
         	$state = true;
         	for($i=0;$i<count($prop);$i+=2)
         	{
-				$result= snmpget($row['DeviceIP'], $row['Community'], $prop[$i+1], $row['Timeout']);
+        		
+				//$result= snmpget($row['DeviceIP'], $row['Community'], $prop[$i+1], $row['Timeout']);
+				$result = $this->parse($prop[$i+1], $row);
+				//echo $result.'\\';
         		if($result)
         			$prop[$i+1] = $result;
         		else
         		{
+        			//$prop[$i+1] = 'ytn';
 					$state = false;	
 					break;
 				}			
@@ -86,12 +125,6 @@ class Monitor_Model extends Model
         	{
 				echo '<div id="d'.$row['DeviceId'].'" class="device_on">';
 				createToolTip($prop);
-				/*$sth = $this->database->prepare("UPDATE devices SET Properties = :prop WHERE DeviceIP = :ip");
-        $sth->execute(array(
-                        ':ip' => $_POST['ip'],
-                        ':prop' =>  json_encode($_POST['prop'])
-                    ));
-        $sth->closeCursor();*/
 			}		
         	else
         		echo '<div id="d'.$row['DeviceId'].'" class="device_off">';
@@ -123,7 +156,10 @@ function createToolTip($prop)
 	//$prop = json_decode($prop);
 	echo '<span class="tooltiptext">';
 	for ($i= 0; $i < count($prop); $i+=2)
+	if(strpos($prop[$i+1],":"))
 		echo $prop[$i].': '.substr($prop[$i+1], strpos($prop[$i+1],":")+1, strlen($prop[$i+1])).'<br/>';
+	else 
+		echo $prop[$i].': '.$prop[$i+1].'<br/>';
 	echo '</span>';
 }
 ?>
